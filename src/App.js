9627bb73d27c9989d3e382e0af732e2da1780d8b
Brandon Campbell-Kearns <bk@brandonkearns.com>
2020-03-05 15:57:26 -1000

    Adds fetch to live JSON server for populating books state object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "Aloha Spark",
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://www.json-generator.com/api/json/get/cgwULHiWDC?indent=2")
+      .then(response => response.json())
+      .then(data => {
+        const books = data;
+        this.setState({books: books});
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.name} Library</h1>
+        <section className="books">
+          {
+            this.state.books.map(book => (
+              <article>
+                <h4>{book.title}</h4>
+                <img src={book.picture}/>
+                <p>by <span>{book.author}</span></p>
+                <p className="pages">{book.pages} pages</p>
+              </article>
+             ) )
+          }
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
